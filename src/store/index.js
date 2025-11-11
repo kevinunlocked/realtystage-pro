ff05d1f868666cf98a9211cbb02de44de2ce6229
kevinunlocked <kevinchisango10@gmail.com>
2025-11-11 02:27:43 +0200

    Fix: Remove TypeScript syntax from JavaScript file for Vercel build

--- src/store/index.js
+++ src/store/index.js
@@ -28,6 +28,8 @@ export const store = configureStore({
     }),
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+// Type exports for TypeScript (if using TypeScript)
+// For JavaScript, these are not needed but can be used for JSDoc
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
 
